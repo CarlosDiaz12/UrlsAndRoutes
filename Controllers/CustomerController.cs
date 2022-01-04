c9c202f2849d7ef357d22237d320f757f949312d
Carlos Diaz <carlos.cedp@gmail.com>
2022-01-04 16:42:37 -0400

    finish chapter 15

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -10,7 +10,11 @@ namespace UrlsAndRoutes.Controllers
     public class CustomerController : Controller
     {
         // GET: Customer
-        [Route("Test")]
+        /*
+         Prefixing the URL with ∼/ tells the MVC Framework that I don’t want the RoutePrefix attribute applied to the Index
+            action method, which means that it will still be accessible through the URL /Test.
+         */
+        [Route("∼/Test")]
         public ActionResult Index()
         {
             ViewBag.Controller = "Customer";
@@ -19,14 +23,14 @@ namespace UrlsAndRoutes.Controllers
         }
 
         // Creating an Attribute Route with a Segment Variable
-        [Route("Users/Add/{user}/{id:int}")]
+        [Route("Add/{user}/{id:int}")]
         public string Create(string user, int id)
         {
             return string.Format("User: {0}, ID: {1}", user, id);
         }
 
         // Adding an Action Method and Route
-        [Route("Users/Add/{user}/{password :alpha:length(6) }")]
+        [Route("Add/{user}/{password}")]
         public string ChangePass(string user, string password)
         {
             return string.Format("ChangePass Method - User: {0}, Pass:{ 1}", user, password);
