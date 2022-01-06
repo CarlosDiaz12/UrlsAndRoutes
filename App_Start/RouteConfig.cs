05bb809a688a7b3a3348a353a225c44359037832
Carlos Diaz <carlos.cedp@gmail.com>
2022-01-06 13:16:58 -0400

    page 418

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,22 +13,26 @@ namespace UrlsAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.RouteExistingFiles = false;
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.MapMvcAttributeRoutes();
+
             //routes.MapRoute("NewRoute", "App/Do{action}",
             //        new { controller = "Home" });
 
-            //routes.MapRoute("MyRoute", "{controller}/{action}/{id}",
-            //        new
-            //        {
-            //            controller = "Home",
-            //            action = "Index",
-            //            id = UrlParameter.Optional
-            //        });
+            routes.Add(new LegacyRoute("~/articles/Windows_3.1_Overview.html", "~/old/.NET_1.0_Class_Library"));
+
+            routes.MapRoute("MyRoute", "{controller}/{action}/{id}",
+                    new
+                    {
+                        controller = "Home",
+                        action = "Index",
+                        id = UrlParameter.Optional
+                    });
 
             // you can select a specific route to be used to generate an outgoing URL
-            routes.MapRoute("MyRoute", "{controller}/{action}");
-            routes.MapRoute("MyOtherRoute", "App/{action}", new { controller = "Home" });
+            //routes.MapRoute("MyRoute", "{controller}/{action}");
+            //routes.MapRoute("MyOtherRoute", "App/{action}", new { controller = "Home" });
         }
     }
 }
